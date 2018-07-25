b07a926631f24a939dc2eeb624253867ebe9a381
James.Woodard <james@jungledisk.com>
2018-07-25 14:51:53 -0500

    added ungraceful exception handling for words with no syllables

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -11,32 +11,63 @@ namespace PigLatin
 
             void TranslateWords() {
                 Console.WriteLine("Please type in a word or sentence and press the Enter key.");
-                string sentence = Console.ReadLine();
+                string sentence = Console.ReadLine().ToLower();
                 string[] words = sentence.Split(' ');
                 string[] translatedWords = new string[words.Length];
-                for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j < words.Length; j++)
                 {
-                    char[] vowels =  { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'y', 'Y'};
-                    string word = words[i];
-                    int wordLength = word.Length;
-                    int firstVowelIndex = word.IndexOfAny(vowels);
-                    string firstHalf = word.Substring(0, firstVowelIndex);
-                    int secondHalfLength = wordLength - firstVowelIndex;
-                    string secondHalf = word.Substring(firstVowelIndex, secondHalfLength);
-                    if (firstVowelIndex == 0)
+                    char[] vowels =  { 'a', 'e', 'i', 'o', 'u', 'y',};
+                    string a = "a";
+                    string e = "e";
+                    string i = "i";
+                    string o = "o";
+                    string u = "u";
+                    string y = "y";
+                    string word = words[j];
+                    int wordLength = word.Length; 
+
+                    if (word.Contains(a) || word.Contains(e) || word.Contains(i) || word.Contains(o) || word.Contains(u) || word.Contains(y))
                     {
-                        string composite = secondHalf + firstHalf + "yay";
-                        translatedWords[i] = composite;
+                        int firstVowelIndex = word.IndexOfAny(vowels);
+                        string firstHalf = word.Substring(0, firstVowelIndex);
+                        int secondHalfLength = wordLength - firstVowelIndex;
+                        string secondHalf = word.Substring(firstVowelIndex, secondHalfLength);
+                        if (firstVowelIndex == 0)
+                        {
+                            string composite = secondHalf + firstHalf + "yay";
+                            translatedWords[j] = composite;
+                        }
+                        else
+                        {
+                            string composite = secondHalf + firstHalf + "ay";
+                            translatedWords[j] = composite;
+                        }
                     }
                     else
                     {
-                        string composite = secondHalf + firstHalf + "ay";
-                        translatedWords[i] = composite;
+                        translatedWords[j] = word + "yay";
                     }
+
+
+                    // int firstVowelIndex = word.IndexOfAny(vowels);
+                    // string firstHalf = word.Substring(0, firstVowelIndex);
+                    // int secondHalfLength = wordLength - firstVowelIndex;
+                    // string secondHalf = word.Substring(firstVowelIndex, secondHalfLength);
+                    // if (firstVowelIndex == 0)
+                    // {
+                    //     string composite = secondHalf + firstHalf + "yay";
+                    //     translatedWords[j] = composite;
+                    // }
+                    // else
+                    // {
+                    //     string composite = secondHalf + firstHalf + "ay";
+                    //     translatedWords[j] = composite;
+                    // }
                 }
                 string translatedSentence = String.Join(" ", translatedWords);
                 Console.WriteLine(translatedSentence);
             }
+
             void PlayAgain() {
                 string[] yes =  { Convert.ToString('y'), Convert.ToString('Y'), "yes", "Yes", "YES"};
                 Console.WriteLine("Do you want to try again? [y/n]");
@@ -55,6 +86,8 @@ namespace PigLatin
                     }
                 }
             }
+
+            
         }
     }
 }
