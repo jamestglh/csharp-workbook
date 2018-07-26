2d8e49a34879c61c40da148c08d4a253b54e03c2
James.Woodard <james@jungledisk.com>
2018-07-26 11:10:55 -0500

    finished project

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,44 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int number = 0;
+            RequestSustenance();
+            BuzzBeast();
+            Console.ReadLine();
+
+            void BuzzBeast(){
+            int i = 0;
+            while (i < number)
+                {
+                    i++;
+                    if (i % 3 == 0 && i % 5 == 0)  
+                    {
+                        Console.WriteLine("FIZZBUZZ, the wild beast howls");
+                        
+                    }
+                    else if (i % 3 == 0){
+                        Console.WriteLine("FIZZ, the creature foams at the mouth");
+                        
+                    }
+                    else if (i % 5 == 0){
+                        Console.WriteLine("BUZZ, the creature emits poisonous clouds from its pores");
+                        
+                    }
+                    else {
+                        Console.WriteLine(i);
+
+                    }
+
+                }
+            }
+
+            
+            void RequestSustenance()
+            {
+                Console.WriteLine("FEED ME AN INTEGER. I SUFFER FROM ANCIENT HUNGER.");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("YES, GOOD, YOUR NUMBER IS " + number);
+            }
+             
     }
-}
+}}
