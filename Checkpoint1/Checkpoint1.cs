776efa1e79d50a6a0dc85535e667f5cdef12e433
James.Woodard <james@jungledisk.com>
2018-07-30 09:22:04 -0500

    started over on the FUCKING CHECKPOINT BECAUSE IM AN IDIOT AND OVERWROTE IT

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Checkpoint1
 {
@@ -6,7 +9,43 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void MainMenu(){
+
+
+            }
+
+            
+            // 1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+            void Problem1(){
+
+                
+            }
+            
+
+            // 2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+            void Problem2(){
+                int playerChoice = 0;
+                Console.WriteLine("Please enter a number, or hit OK if you are done entering numbers");
+            }
+
+
+            // 3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+            void problem3(){
+
+
+            }
+
+            // 4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+            void problem4(){
+
+                
+            }
+
+            // 5- Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+            void problem5(){
+
+                
+            }
         }
     }
 }
