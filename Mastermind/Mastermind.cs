2c4731db5421c2d2878af0a2d36ffa458b864b54
James.Woodard <james@jungledisk.com>
2018-08-19 11:24:15 -0500

    added comments and finished hanoi

--- Mastermind/Mastermind.cs
+++ Mastermind/Mastermind.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Threading;
 
 
-namespace Mastermind {
-    class Program {
+namespace Mastermind 
+{
+    class Program 
+    {
         static void Main (string[] args) {
  	        
             string[] randomLetters = new string[] {"a", "b", "c", "d", "e", "f"};
             Random rando = new Random();
-            Game game = new Game (new string[] { randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)] });
-            for (int turns = 10; turns > 0; turns--) {
+            Game game = new Game (new string[] { randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)], randomLetters[rando.Next(0,6)] }, 10);
+            // Game game = new Game (new string[] { "a", "a", "a", "a" });
+            for (int turns = game.numTries; turns > 0; turns--) {
                 Console.WriteLine($"You have {turns} tries left");
                 Console.WriteLine ("Choose four letters: ");
                 string letters = Console.ReadLine ();
@@ -33,7 +36,10 @@ namespace Mastermind {
         private List<Row> rows = new List<Row> ();
         private string[] answer = new string[4];
 
-        public Game (string[] answer) {
+        public int numTries {get;set;}
+
+        public Game (string[] answer, int numTries) 
+        {
             this.answer = answer;
         }
 
@@ -44,24 +50,28 @@ namespace Mastermind {
             // this.answer => ["a", "b", "c", "d"]
             // row.balls => [{ Letter: "c" }, { Letter: "b" }, { Letter: "d" }, { Letter: "a" }]
             int red = 0;
-            for (int i = 0; i < 4; i++) {
-                if (answerClone[i] == row.balls[i].Letter) {
+            for (int i = 0; i < 4; i++) 
+            {
+                if (answerClone[i] == row.balls[i].Letter) 
+                {
                     red++;
                 }
             }
 
             int white = 0;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) 
+            {
                 int foundIndex = Array.IndexOf (answerClone, row.balls[i].Letter);
                 if (foundIndex > -1) {
                     white++;
                     answerClone[foundIndex] = null;
                 }
             }
-            if (white - red == 0)
+            //win conditions
+            if ((white - red == 0) && (red == 4))
             {
                 Console.WriteLine("You've won!");
-                // System.Environment.Exit(1);
+                System.Environment.Exit(1);
             }
 
 
@@ -69,13 +79,17 @@ namespace Mastermind {
             
         }
 
-        public void AddRow (Row row) {
+        public void AddRow (Row row) 
+        {
             this.rows.Add (row);
         }
 
-        public string Rows {
-            get {
-                foreach (var row in this.rows) {
+        public string Rows 
+        {
+            get 
+            {
+                foreach (var row in this.rows) 
+                {
                     Console.WriteLine (row.Balls);
                     Console.WriteLine (Score (row));
                 }
@@ -84,24 +98,31 @@ namespace Mastermind {
         }
     }
 
-    class Ball {
+    class Ball 
+    {
         public string Letter { get; private set; }
 
-        public Ball (string letter) {
+        public Ball (string letter) 
+        {
             this.Letter = letter;
         }
     }
 
-    class Row {
+    class Row 
+    {
         public Ball[] balls = new Ball[4];
 
-        public Row (Ball[] balls) {
+        public Row (Ball[] balls) 
+        {
             this.balls = balls;
         }
 
-        public string Balls {
-            get {
-                foreach (var ball in this.balls) {
+        public string Balls 
+        {
+            get 
+            {
+                foreach (var ball in this.balls) 
+                {
                     Console.Write (ball.Letter);
                 }
                 return "";
--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -11,19 +11,112 @@ namespace TowersOfHanoi
         {
             Console.WriteLine("Towers of Hanoi");
             Game myGame = new Game();
-            //add myGame.run()
-            //playagain
-            
+            myGame.Run();
         }
-
     }
 
 
     public class Game{
         Dictionary<string, Tower> towers = new Dictionary<string, Tower>();
+                bool hasWon = false;
+                string location = "";
+                string destination = "";
 
-        public Game(){
-             
+            void GetPlayerInput()
+            {
+                Console.WriteLine("What stack would you like to move the block from?");
+                string userInput = Console.ReadLine();
+                
+                if ((userInput == "a") ||(userInput == "A") || (userInput == "b") ||(userInput == "B") || (userInput == "c") ||(userInput == "C"))
+                {
+                    location = userInput.ToUpper();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, your input is invalid.");
+                    userInput = null;
+                    location = null;
+                    destination = null;
+                    GetPlayerInput();
+                }
+                Console.WriteLine("What stack would you like to move the block to?");
+                userInput = Console.ReadLine();
+                if ((userInput == "a") ||(userInput == "A") || (userInput == "b") ||(userInput == "B") || (userInput == "c") ||(userInput == "C"))
+                {
+                    destination = userInput.ToUpper();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, your input is invalid.");
+                    userInput = null;
+                    location = null;
+                    destination = null;
+                    GetPlayerInput();
+                }
+            }
+            void MovePiece(){
+                
+                if (towers[location].blockStack.Count == 0) //checks if "from" input is empty
+                {
+                    Console.WriteLine("Sorry, that move is invalid");
+                    return;
+                }
+                var pieceToMove = towers[location].blockStack.Peek();
+                if (towers[destination].blockStack.Count == 0) //checks if destination is empty
+                {
+                    towers[location].blockStack.Pop();
+                    towers[destination].blockStack.Push(pieceToMove);
+                }
+                else if (towers[destination].blockStack.Peek().weight > pieceToMove.weight) //checks to see if block in destination is bigger
+                {
+                    towers[location].blockStack.Pop();
+                    towers[destination].blockStack.Push(pieceToMove);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that move is invalid"); //if user tries to put a bigger block on a smaller one
+                }
+            }
+            
+            void PrintBoard(){
+            foreach (string name in towers.Keys)
+            {
+                Console.Write(name + ": ");
+                Tower tower = towers[name]; 
+                Stack<Block> stackOfBlocks = tower.blockStack;
+                Stack<Block> stackToPrint = new Stack<Block>();
+
+                foreach(Block b in stackOfBlocks) // this loop dumps the stack into a new stack, so it's printed out the proper way
+                {
+                    stackToPrint.Push(b);
+                }
+                foreach (Block z in stackToPrint) // this loop prints out the stack in the proper order
+                {
+                    Console.Write(z.weight);  
+                }
+                Console.WriteLine();
+                }
+            }
+
+            void CheckForWin()
+            {
+                if (towers["C"].blockStack.Count >= 4){
+                    Console.WriteLine("You Win!");
+                    hasWon = true;
+                }
+            }
+            public void Run() // this is the game logic
+            {
+                while (!hasWon)
+                {
+                    PrintBoard();
+                    GetPlayerInput();
+                    MovePiece();
+                    CheckForWin();
+                }
+            }
+
+        public Game(){ //constructor for Game
             towers["A"] = new Tower();
             towers["B"] = new Tower();
             towers["C"] = new Tower();
@@ -37,128 +130,14 @@ namespace TowersOfHanoi
             towers["A"].blockStack.Push(block3);
             towers["A"].blockStack.Push(block2);
             towers["A"].blockStack.Push(block1);
-            //this is where constructor should end
-            bool hasWon = false;
-            string location = "";
-            string destination = "";
-
-            while (!hasWon)
-            {
-                PrintBoard();
-                PlayerMove();
-                CheckForWin();
-            
-            }
-
-            void PrintBoard(){
-                foreach (string name in towers.Keys)
-                {
-                    Console.Write(name + ": ");
-                    Tower tower = towers[name]; 
-                    Stack<Block> stackOfBlocks = tower.blockStack;
-                    
-                     //FINISH THIS
-                }
-                // Console.Write("A: ");
-                // foreach (Block block in towers["A"].blockStack)
-                //     {
-                //        Console.Write(block.weight);
-                //     }
-                //     Console.WriteLine();
-                // Console.Write("B: ");
-                // foreach (Block block in towers["B"].blockStack)
-                //     {
-                //        Console.Write(block.weight);
-                //     }
-                //     Console.WriteLine();
-                // Console.Write("C: ");
-                // foreach (Block block in towers["C"].blockStack)
-                //     {
-                //        Console.Write(block.weight);
-                //     }
-                //     Console.WriteLine();
-            }
-
-            void CheckForWin(){
-                if (towers["C"].blockStack.Count >= 4){
-                    Console.WriteLine("You Win!");
-                    hasWon = true;
-                }
-            }
-
-            void PlayerMove(){
-
-                // this gets player input
-                // string[] acceptableLetters = new string[] {"a", "A", "b", "B", "c", "C"};
-                Console.WriteLine("What stack would you like to move the block from?");
-                string userInput = Console.ReadLine();
-                
-                    if ((userInput == "a") ||(userInput == "A") || (userInput == "b") ||(userInput == "B") || (userInput == "c") ||(userInput == "C"))
-                    {
-                        location = userInput.ToUpper();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Sorry, your input is invalid.");
-                        userInput = null;
-                        location = null;
-                        destination = null;
-                        return;
-                    }
-                
-                Console.WriteLine("What stack would you like to move the block to?");
-                userInput = Console.ReadLine();
-
-
-                    if ((userInput == "a") ||(userInput == "A") || (userInput == "b") ||(userInput == "B") || (userInput == "c") ||(userInput == "C"))
-                    {
-                        destination = userInput.ToUpper();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Sorry, your input is invalid.");
-                        userInput = null;
-                        location = null;
-                        destination = null;
-                        return;
-                    }
-
-
-
-                //this moves the pieces
-                var pieceToMove = towers[location].blockStack.Peek();
-                if (towers[destination].blockStack.Count == 0) 
-                {
-                    towers[location].blockStack.Pop();
-                    towers[destination].blockStack.Push(pieceToMove);
-                }
-                else if (towers[destination].blockStack.Peek().weight > pieceToMove.weight)
-                {
-                    towers[location].blockStack.Pop();
-                    towers[destination].blockStack.Push(pieceToMove);
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, that move is invalid");
-                }
-            }
-
-            // void MovePiece(){
-            //     var pieceToMove = towers[location.ToUpper()].blockStack.Pop();
-            //     towers[destination.ToUpper()].blockStack.Push(pieceToMove);
-            // }
-
-
         }
     }
     public class Tower{
         public Stack<Block> blockStack {get;set;}
-
-        public Tower(){
-
+        public Tower()
+            {
                 this.blockStack = new Stack<Block>();
             }
-
     }
     public class Block{
         public int weight {get; set;}
