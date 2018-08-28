34b1e1654cfdebdf48a81dad792ee03aaedafe14
James.Woodard <james@jungledisk.com>
2018-08-28 12:16:26 -0500

    finished enum project

---
+++ enum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace enumpractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the MONTH you were born in, as a number: ");
+            string dateMonth = Console.ReadLine();
+            int dateMonthInt = Convert.ToInt32(dateMonth);
+
+            Console.WriteLine("Please enter what DAY of the month you were born on, as a number: ");
+            string dateDay = Console.ReadLine();
+            int dateDayInt = Convert.ToInt32(dateDay);
+
+            Console.WriteLine("Please enter ANY YEAR to see what day of the month your birthday lands on in any given year: ");
+            string dateYear = Console.ReadLine();
+            int dateyearInt = Convert.ToInt32(dateYear);
+
+            DateTime dt = new DateTime(dateyearInt, dateMonthInt, dateDayInt);
+            int dayOfWeekInt = (int)dt.DayOfWeek;
+
+            Console.WriteLine("The day of the week for {0:d} is {1}.", dt, (Days)dayOfWeekInt);
+        }
+    }
+    public enum Days 
+    {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+
+
+
+}
+
+
--- practice2/Program.cs
+++ practice2/Program.cs
@@ -39,18 +39,7 @@ namespace practice2
     }
 
 
-    //EXAMPLE FROM THE INNERNETTE
-    // class Sample 
-    // {
-    //     public static void Main() 
-    //     {
-    // // Assume the current culture is en-US.
-    // // Create a DateTime for the first of May, 2003.
-    //     DateTime dt = new DateTime(2003, 5, 1);
-    //     Console.WriteLine("Is Thursday the day of the week for {0:d}?: {1}", 
-    //                     dt, dt.DayOfWeek == DayOfWeek.Thursday);
-    //     Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
-    //     }
+
 }
-/*
-}
+
+
